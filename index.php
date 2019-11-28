097cc60f286bf28ef2e2748c1b02538700328cf1
Mathieu Daix <mathieudaixpro@gmail.com>
2019-11-28 17:55:03 +0100

    add styles

--- index.php
+++ index.php
@@ -19,19 +19,27 @@ include_once('dir.php');
 
 include_once('WeaponManager.php');
 
-// function fight(array &$gangsters, Weapon $weapon)
-// {
-//     $weapon->shoot($gangsters);
-// }
+echo "\e[91m \e[1m                                                      \n";
+echo "                       Welcome to                     \n";
 
-// fight($gangsters, WeaponFactory::getWeapon('Revolver'));
+echo "                                                      \n";
+echo "   _____                 _                          _ \n";
+echo "  / ____|               | |                        | |\n";
+echo " | (___   ___  _ __ ___ | |__  _ __ ___ _ __ ___   | |\n";
+echo "  \___ \ / _ \| '_ ` _ \| '_ \| '__/ _ \ '__/ _ \  | |\n";
+echo "  ____) | (_) | | | | | | |_) | | |  __/ | | (_) | |_|\n";
+echo " |_____/ \___/|_| |_| |_|_.__/|_|  \___|_|  \___/  (_)\n";
+echo "                                                      \n";
+echo "\e[4m                                                      \e[0m\n";
+
+while ($count < 2 || $count > 6) {
+    echo "\e[1m                                                      \n";
+    $count = Cliutil::getFromCli('How many gangsters are you? ');
+    echo "\e[0m                                                      \n";
+}
 
 $gangsters = [];
 $count = 0;
-
-while ($count < 2 || $count > 6) {
-    $count = Cliutil::getFromCli('How many gangsters are you? ');
-}
 
 for ($i = 0; $i < $count; $i++) {
     $gangsters[] = gangsterManager::createGangsterFromCli();
